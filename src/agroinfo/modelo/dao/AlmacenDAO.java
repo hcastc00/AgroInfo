522866b9ed1346ed92b669c6f54340ff9c6fb153
rgonzg17 <rgonzg17@estudiantes.unileon.es>
2021-01-12 18:56:33 +0100

    Modificado almacén, sus métodos sin lanzar expeciones

--- src/agroinfo/modelo/dao/AlmacenDAO.java
+++ src/agroinfo/modelo/dao/AlmacenDAO.java
@@ -13,26 +13,31 @@ public class AlmacenDAO extends ConexionBD {
     public AlmacenDAO() {
     }
 
-    public Almacen getAlmacen() throws SQLException {
+    public Almacen getAlmacen(){
 
         Almacen almacen = null;
 
         this.abrirConexion();
 
-        ResultSet rs = this.getConnection().createStatement().executeQuery("SELECT * FROM almacen WHERE id = 1");
-        almacen = new Almacen();
-        almacen.setConejos(rs.getInt(1));
-        almacen.setPiensoLactancia(rs.getDouble(2));
-        almacen.setPiensoMedicado(rs.getDouble(3));
-        almacen.setPiensoRemate(rs.getDouble(4));
-        almacen.setExcedenteTotal(rs.getInt(5));
+
+        try {
+            ResultSet rs = this.getConnection().createStatement().executeQuery("SELECT * FROM almacen WHERE id = 1");
+            almacen = new Almacen();
+            almacen.setConejos(rs.getInt(1));
+            almacen.setPiensoLactancia(rs.getDouble(2));
+            almacen.setPiensoMedicado(rs.getDouble(3));
+            almacen.setPiensoRemate(rs.getDouble(4));
+            almacen.setExcedenteTotal(rs.getInt(5));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
         this.cerrarConexion();
 
         return almacen;
     }
 
-    public void modificar(Almacen almacen, String usuario_identificador) throws SQLException {
+    public void modificar(Almacen almacen, String usuario_identificador){
 
         this.abrirConexion();
 
@@ -44,13 +49,17 @@ public class AlmacenDAO extends ConexionBD {
                 " excedente_parcelas = ?" +
                 " WHERE id = 1";
 
-        PreparedStatement pSentencia = this.getConnection().prepareStatement(sentencia);
-        pSentencia.setInt   (1, almacen.getConejos());
-        pSentencia.setDouble(2, almacen.getPiensoLactancia());
-        pSentencia.setDouble(3, almacen.getPiensoMedicado());
-        pSentencia.setDouble(4, almacen.getPiensoRemate());
-        pSentencia.setInt   (5, almacen.getExcedenteTotal());
-        pSentencia.executeUpdate();
+        try {
+            PreparedStatement pSentencia = this.getConnection().prepareStatement(sentencia);
+            pSentencia.setInt   (1, almacen.getConejos());
+            pSentencia.setDouble(2, almacen.getPiensoLactancia());
+            pSentencia.setDouble(3, almacen.getPiensoMedicado());
+            pSentencia.setDouble(4, almacen.getPiensoRemate());
+            pSentencia.setInt   (5, almacen.getExcedenteTotal());
+            pSentencia.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
         RegistroDAO.registrar(this.getConnection(), usuario_identificador,
                 "El usuario ha modificado elementos del almacen",
