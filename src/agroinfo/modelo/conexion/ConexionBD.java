ee07e9ca26b2c52fba34658f81cbdac4848e263e
DiegoGonL <dgonzl05@estudiantes.unileon.es>
2021-01-12 19:13:23 +0100

    Eliminados setters de ConexionBD

--- src/agroinfo/modelo/conexion/ConexionBD.java
+++ src/agroinfo/modelo/conexion/ConexionBD.java
@@ -22,44 +22,22 @@ public class ConexionBD {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getDriverName() {
         return driverName;
-    }
-
-    public void setDriverName(String driverName) {
-        this.driverName = driverName;
     }
 
     public String getUser() {
         return user;
     }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
-
     public String getPass() {
         return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
     }
 
     public Connection getConnection() {
         return connection;
     }
 
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
-
-
-    //TODO quitar los try catch y propagar la excepcion para que se controle en la vista
     public void abrirConexion() {
 
         if (this.url.isBlank() || this.driverName.isBlank() || this.user.isBlank() || this.pass.isBlank()) {
